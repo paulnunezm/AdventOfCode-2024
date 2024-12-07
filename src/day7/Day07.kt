d730e57f48ef0411f00a386d95cb4c5f2620b56d
Paul Nunez <v-paul.nunez@pinger.com>
2024-12-07 14:56:52 +0100

    day 7 part 2 solution

--- src/day7/Day07.kt
+++ src/day7/Day07.kt
@@ -4,6 +4,7 @@ import println
 import readInput
 import readInputForDay
 import readTestInputForDay
+import kotlin.system.exitProcess
 import kotlin.system.measureTimeMillis
 
 private data class Equation(val result: Long, val values: List<Long>)
@@ -12,13 +13,11 @@ fun main() {
     val testInput = transformInput(readTestInputForDay(7, part = 1))
     val input = transformInput(readInputForDay(7))
 
-    part1(testInput).also {
-        check(it == 3749L) { "test result was $it" }
-    }
+    part1(testInput).also { check(it == 3749L) { "test result was $it" } }
+    part2(testInput).also { check(it == 11387L) { "test result was $it" } }
 
-    measureTimeMillis {
-        "Part 1: ${part1(input)}".println() // Result: 6242
-    }.also { "took $it ms".println() } // 9ms
+    "Part 1: ${part1(input)}".println() // Result: 12553187650171
+    "Part 2: ${part2(input)}".println() // Result: 96779702119491
 }
 
 private fun part1(input: List<Equation>): Long =
@@ -30,6 +29,39 @@ private fun part1(input: List<Equation>): Long =
             0
         }
     }
+
+private fun part2(input: List<Equation>): Long {
+    val part1Result = part1(input)
+
+    return input.filterNot {
+        val permutations = operationPermutation(it)
+        it.result in permutations
+    }.sumOf { equation ->
+        val permutations = operationConcatPermutation(equation)
+        if (equation.result in permutations) {
+            equation.result
+        } else {
+            0
+        }
+    } + part1Result
+}
+
+private fun operationConcatPermutation(equation: Equation): List<Long> {
+    val permutations = equation.values.fold(emptyList()) { acc: List<Long>, i: Long ->
+        if (acc.isEmpty()) {
+            return@fold listOf(i)
+        } else {
+            val x = mutableListOf<Long>()
+            for (j in acc.indices) {
+                x += acc[j] + i
+                x += acc[j] * i
+                x += "${acc[j]}$i".toLong()
+            }
+            return@fold x
+        }
+    }
+    return permutations
+}
 
 private fun operationPermutation(equation: Equation): List<Long> {
     val permutations = equation.values.fold(emptyList()) { acc: List<Long>, i: Long ->
