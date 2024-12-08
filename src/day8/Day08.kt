09383bc7ac35330e4b1d77aad27af0d1b36ea6cc
Paul Nunez <v-paul.nunez@pinger.com>
2024-12-09 00:28:37 +0100

    day 8 solution

--- src/day8/Day08.kt
+++ src/day8/Day08.kt
@@ -3,151 +3,96 @@ package day8
 import println
 import readInputForDay
 import readTestInputForDay
-import kotlin.io.path.Path
-import kotlin.io.path.readText
 import kotlin.math.abs
-import kotlin.system.measureTimeMillis
-
 
 fun main() {
     val testInput = readTestInputForDay(8, 1)
     val testMatrix = parseMatrix(testInput)
-    testMatrix.map {
-        it.joinToString(separator = "") { it.toString() }
-    }.onEach { println(it) }
-    "\n\n".println()
-    part1(testMatrix)
+    drawMatrix(testMatrix, emptyList())
+    part1(testMatrix).println()
 
     val input = readInputForDay(8)
-    "\n\n".println()
-    Path("src/day8/input_8.txt").readText().trim().lines().onEach(::println)
-    "\n\n".println()
-    measureTimeMillis {
-        val inputMatrix = parseMatrix(input)
-        "Part 1: ${part1(inputMatrix)}".println() // Result:
-    }.also { "took $it ms".println() } // 12ms
+    val inputMatrix = parseMatrix(input)
+    "Part 1: ${part1(inputMatrix)}".println() // Result: 396
 }
 
-data class Position(val row: Int, val col: Int)
+private data class Position(val row: Int, val col: Int)
 
-private fun parseMatrix(input: List<String>) =
-    input.map { it.toList() }
-
-private fun Char.isAntenna() = this.isLetterOrDigit()
-
-private fun goTroughMatrix(matrix: List<List<Char>>, block: (element: Char, row: Int, col: Int) -> Unit) {
-    for (row in matrix.indices) {
-        for (col in matrix[0].indices) {
-            block(matrix[row][col], row, col)
-        }
-    }
-}
+private fun parseMatrix(input: List<String>) = input.map { it.toList() }
 
 private fun getAntennasMap(matrix: List<List<Char>>): MutableMap<Char, List<Position>> {
     val antennasMap = mutableMapOf<Char, List<Position>>()
-    goTroughMatrix(matrix) { element, row, col ->
-        if (element.isAntenna()) {
-            val elementPos = Position(row, col)
-            val antennaPositions = antennasMap[element] ?: emptyList()
-            antennasMap[element] = antennaPositions + elementPos
+    for (row in matrix.indices) {
+        for (col in matrix[0].indices) {
+            val element = matrix[row][col]
+            if (element.isLetterOrDigit()) { // is antenna
+                val elementPos = Position(row, col)
+                val antennaPositions = antennasMap[element] ?: emptyList()
+                antennasMap[element] = antennaPositions + elementPos
+            }
         }
     }
     return antennasMap
 }
 
-private fun Position.ifIsBetweenBounds(matrixLastIndex: Int): Boolean =
-    this.row in 0..matrixLastIndex && this.col in 0..matrixLastIndex
+private fun Position.ifIsBetweenBounds(indices: IntRange): Boolean =
+    this.row in indices && this.col in indices
 
 private operator fun Position.minus(b: Position): Position =
-    Position(
-        this.row - b.row,
-        this.col - b.col,
-    )
+    Position(this.row - b.row, this.col - b.col)
 
-private fun Position.absolute() =
-    Position(
-        abs(this.row),
-        abs(this.col)
-    )
+private fun Position.absolute() = Position(abs(this.row), abs(this.col))
 
 private fun getAntiNodesForFirstPosition(
-    antennasPositions: Set<Position>,
-    matrixLastIndex: Int,
+    frequenciesPositions: Set<Position>,
     positions: List<Position>,
     antiNodes: MutableSet<Position>
 ) {
 
     // Base case
-    if (positions.isEmpty()) return
-
-//    "\n === new interation".println()
+    if (positions.size == 1 || positions.isEmpty()) return
 
     // Pre-recursion
     val current = positions.first()
     val positionsLeft = positions.drop(1)
-//    "current $current, left: $positionsLeft".println()
 
     for (position in positionsLeft) {
         val diff = (current - position).absolute()
-        val beforeAntenna = if (current.col >= position.col) {
-            Position(
-                current.row - diff.row,
-                current.col + diff.col
-            )
-        } else {
-            Position(
-                current.row - diff.row,
-                current.col - diff.col
-            )
-        }
-        val afterAntenna = if (current.col >= position.col) {
-            Position(
-                position.row + diff.row,
-                position.col - diff.col
-            )
-        } else {
-            Position(
-                position.row + diff.row,
-                position.col + diff.col
-            )
-        }
 
-        if (beforeAntenna.ifIsBetweenBounds(matrixLastIndex)
-//            && !antennasPositions.contains(beforeAntenna)
-        ) {
-            antiNodes.add(beforeAntenna)
-        }
-        if (afterAntenna.ifIsBetweenBounds(matrixLastIndex)
-//            && !antennasPositions.contains(afterAntenna)
-        ) {
-            antiNodes.add(afterAntenna)
-        }
+        val isCurrentToTheRight = current.col >= position.col
+        val beforeAntiNodeColPos = if (isCurrentToTheRight) {
+            current.col + diff.col
+        } else current.col - diff.col
+
+        val beforeAntiNode = Position(
+            current.row - diff.row,
+            beforeAntiNodeColPos
+        )
+        val afterAntiNodeColPos = if (isCurrentToTheRight) {
+            position.col - diff.col
+        } else position.col + diff.col
+
+        val afterAntiNode = Position(
+            position.row + diff.row,
+            afterAntiNodeColPos,
+        )
+        antiNodes.add(beforeAntiNode)
+        antiNodes.add(afterAntiNode)
     }
 
-    getAntiNodesForFirstPosition(antennasPositions, matrixLastIndex, positionsLeft, antiNodes)
+    getAntiNodesForFirstPosition(frequenciesPositions, positionsLeft, antiNodes)
 }
 
 private fun getAntiNodesForAntenna(
-    antennasPositions: Set<Position>,
-    matrixLastIndex: Int,
+    frequenciesPositions: Set<Position>,
     positions: List<Position>
 ): MutableSet<Position> {
     val antiNodes = mutableSetOf<Position>()
-    getAntiNodesForFirstPosition(antennasPositions, matrixLastIndex, positions, antiNodes)
+    getAntiNodesForFirstPosition(frequenciesPositions, positions, antiNodes)
     return antiNodes
 }
 
-private fun part1(matrix: List<List<Char>>): Int {
-    val antennasMap = getAntennasMap(matrix)
-    val antennasPositions = antennasMap.values.flatten().toSet()
-    val antiNodes = mutableListOf<Position>()
-
-    var s = 0
-    for (antenna in antennasMap) {
-        antiNodes += getAntiNodesForAntenna(antennasPositions, matrix.lastIndex, antenna.value)
-    }
-//    antiNodes.count().println()
-
+private fun drawMatrix(matrix: List<List<Char>>, antiNodes: List<Position>) {
     val finalMatrix = matrix.map {
         it.toMutableList()
     }.toMutableList()
@@ -158,5 +103,18 @@ private fun part1(matrix: List<List<Char>>): Int {
     finalMatrix.map {
         it.joinToString(separator = "") { it.toString() }
     }.onEach { println(it) }
+}
+
+private fun part1(matrix: List<List<Char>>): Int {
+    val antennasMap = getAntennasMap(matrix)
+    val frequenciesPositions = antennasMap.values.flatten().toSet()
+    val antiNodes = antennasMap.map {
+        getAntiNodesForAntenna(frequenciesPositions, it.value)
+    }.flatten().filter {
+        it.ifIsBetweenBounds(matrix.indices)
+    }.distinct()
+
+    drawMatrix(matrix, antiNodes)
+
     return antiNodes.count()
 }
